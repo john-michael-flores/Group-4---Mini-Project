9c1f0a38cad45c5c4e8e3a2493ab5b05237093b8
Dean Angelo Vonn Enalpe <vonnenalpe@gmail.com>
2022-11-16 16:19:05 +0800

    Add OAuth2

---
+++ src/main/java/com/miniproject/group4/model/User.java
@@ -0,0 +1,24 @@
+package com.miniproject.group4.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column
+    private String userName;
+    @Column
+    private String password;
+
+
+}
---
+++ src/main/java/com/miniproject/group4/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.miniproject.group4.repository;
+
+
+import com.miniproject.group4.model.User;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends PagingAndSortingRepository<User, Long> {
+    User findByUserName(String userName);
+}
