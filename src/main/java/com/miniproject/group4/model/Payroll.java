bed439c4ca2c34148425ed7cf171e48d7bb246de
Dean Angelo Vonn Enalpe <vonnenalpe@gmail.com>
2022-11-17 16:53:41 +0800

    model and config

--- src/main/java/com/miniproject/group4/model/Payroll.java
+++ src/main/java/com/miniproject/group4/model/Payroll.java
@@ -4,6 +4,7 @@ import com.miniproject.group4.enums.PayrollTypes;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
@@ -12,6 +13,7 @@ import java.math.BigDecimal;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class Payroll {
 
     @Id
